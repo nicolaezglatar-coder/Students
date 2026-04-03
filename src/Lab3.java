46b27849e949ba146af0b211e1a99e2f1d246a19
Nicolae <nicolae.zglatar@ulbsibiu.ro>
2026-04-03 08:56:58 +0300

    lab 2,3

---
+++ src/Lab3.java
@@ -0,0 +1,31 @@
+import java.io.*;
+import java.nio.file.*;
+import java.util.*;
+
+public class Lab3 {
+    public static void main(String[] args) throws IOException {
+
+        List<String> lines = Files.readAllLines(Paths.get("in.txt"));
+
+        // a)
+        List<String> a = new ArrayList<>();
+        for (String line : lines) {
+            a.add(line + "\n");
+        }
+
+        // b)
+        List<String> b = new ArrayList<>();
+        for (String line : lines) {
+            b.add(line.replace(".", ".\n"));
+        }
+
+        // c)
+        List<String> out = new ArrayList<>();
+        out.addAll(a);
+        out.addAll(b);
+
+        Files.write(Paths.get("out.txt"), out);
+
+        System.out.println("Done!");
+    }
+}
---
+++ src/ro/ulbs/proiectaresoftware/students/Lab2.java
@@ -0,0 +1,44 @@
+import java.util.*;
+
+public class Lab2 {
+    public static void main(String[] args) {
+
+        List<Integer> x = new ArrayList<>();
+        List<Integer> y = new ArrayList<>();
+        Random r = new Random();
+
+        for (int i = 0; i < 5; i++) x.add(r.nextInt(11));
+        for (int i = 0; i < 7; i++) y.add(r.nextInt(11));
+
+        Collections.sort(x);
+        Collections.sort(y);
+
+        System.out.println("x = " + x);
+        System.out.println("y = " + y);
+
+        // a) x + y
+        List<Integer> xPlusY = new ArrayList<>(x);
+        xPlusY.addAll(y);
+        Collections.sort(xPlusY);
+
+        // b) intersectie
+        Set<Integer> zSet = new TreeSet<>(x);
+        zSet.retainAll(y);
+
+        // c) x - y
+        List<Integer> xMinusY = new ArrayList<>(x);
+        xMinusY.removeAll(y);
+
+        // d) <= p
+        int p = 4;
+        List<Integer> limited = new ArrayList<>();
+        for (int val : xPlusY) {
+            if (val <= p) limited.add(val);
+        }
+
+        System.out.println("x+y = " + xPlusY);
+        System.out.println("intersectie = " + zSet);
+        System.out.println("x-y = " + xMinusY);
+        System.out.println("<= p = " + limited);
+    }
+}
